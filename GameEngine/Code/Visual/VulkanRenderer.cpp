fa4bfdf264a044455ff76ab2c7f39297fb25c912
Maksym Protsyk <maksprotsyk@gmail.com>
2025-02-17 20:41:01 +0200

    * added fences to fix vulkan crashes

--- GameEngine/Code/Visual/VulkanRenderer.cpp
+++ GameEngine/Code/Visual/VulkanRenderer.cpp
@@ -134,6 +134,10 @@ namespace Engine::Visual
 	void VulkanRenderer::render()
 	{
 
+		// Wait for the fence of the current frame before reusing the command buffer
+		vkWaitForFences(m_device, 1, &m_inFlightFences[m_currentImageInFlight], VK_TRUE, UINT64_MAX);
+		vkResetFences(m_device, 1, &m_inFlightFences[m_currentImageInFlight]);
+
 		auto commandBuffer = m_commandBuffers[m_imageIndex];
 
 		vkCmdEndRenderPass(commandBuffer);
@@ -157,7 +161,7 @@ namespace Engine::Visual
 		submitInfo.pSignalSemaphores = signalSemaphores;
 
 
-		if (vkQueueSubmit(m_graphicsQueue, 1, &submitInfo, nullptr) != VK_SUCCESS) {
+		if (vkQueueSubmit(m_graphicsQueue, 1, &submitInfo, m_inFlightFences[m_currentImageInFlight]) != VK_SUCCESS) {
 			throw std::runtime_error("Failed to submit draw command buffer!");
 		}
 
@@ -372,14 +376,20 @@ namespace Engine::Visual
 	{
 		m_imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
 		m_renderFinishedSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+		m_inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
 
 		VkSemaphoreCreateInfo semaphoreInfo{};
 		semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
 
+		VkFenceCreateInfo fenceInfo{};
+		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+		fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
 
 			if (vkCreateSemaphore(m_device, &semaphoreInfo, nullptr, &m_imageAvailableSemaphores[i]) != VK_SUCCESS ||
-				vkCreateSemaphore(m_device, &semaphoreInfo, nullptr, &m_renderFinishedSemaphores[i]) != VK_SUCCESS) {
+				vkCreateSemaphore(m_device, &semaphoreInfo, nullptr, &m_renderFinishedSemaphores[i]) != VK_SUCCESS ||
+				vkCreateFence(m_device, &fenceInfo, nullptr, &m_inFlightFences[i]) != VK_SUCCESS) {
 				throw std::runtime_error("Failed to create synchronization object for frame!");
 			}
 		}
